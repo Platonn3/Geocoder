32fc3202b77de908f0390d56156a46360dfb3d32
Platonn3 <platon.kurbatov87@gmail.com>
2025-04-28 20:02:47 +0500

    Add new logic

--- parsing.py
+++ parsing.py
@@ -1,5 +1,10 @@
 import response as req
 def get_address():
-    print("Введите адреc:")
-    address = input()
+    print("Введите город:")
+    city = input()
+    print("Введите улицу:")
+    street = input()
+    print("Введите номер дома:")
+    number = input()
+    address = f"{street} {number} {city}"
     req.get_full_address(address)
--- response.py
+++ response.py
@@ -5,19 +5,30 @@ def get_full_address(address):
     headers = {
         "User-Agent": "MyGeocoderApp/1.0 (platon.kurbatov87@gmail.com)"
     }
-    params = {'q': address,
-              'format': 'json',
+    params = {"q": address,
+              "format": "json",
               "limit": 1,
               "accept-language": "ru"
               }
-    response = requests.get(
-        "https://nominatim.openstreetmap.org/search",
-        params=params,
-        headers=headers,
-        timeout=10
-    )
-    if response.ok:
-        if response.text.strip():
-            data = response.json()
-            if data:
-                print(data[0])
+    try:
+        response = requests.get(
+            "https://nominatim.openstreetmap.org/search",
+            params=params,
+            headers=headers,
+            timeout=10
+        )
+        if response.ok:
+            if response.text.strip():
+                data = response.json()
+                if data:
+                    print(data[0])
+                else:
+                    print("Адрес не найден")
+            else:
+                print("Пустой ответ от сервера")
+        else:
+            print(f"Ошибка HTTP: {response.status_code}")
+
+    except requests.exceptions.RequestException as e:
+        print(f"Ошибка запроса {e}")
+
